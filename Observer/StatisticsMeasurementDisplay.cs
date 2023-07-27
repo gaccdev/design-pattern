4b468be11ad4ae72d36c5ad9edf3cbb521b4d559
gaccdev <>
2023-07-27 02:20:47 +0200

    observer pattern implementation

---
+++ Observer/StatisticsMeasurementDisplay.cs
@@ -0,0 +1,36 @@
+﻿namespace Observer
+{
+    public class StatisticsMeasurementDisplay : IObserver, IDisplayElement
+    {
+        private float _temperature;
+        private float _humidity;
+        private float _pressure;
+
+        private readonly WeatherData _weatherData;
+
+        public StatisticsMeasurementDisplay(WeatherData weatherData)
+        {
+            _weatherData = weatherData;
+            _weatherData.RegisterObserver(this);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Display from {nameof(StatisticsMeasurementDisplay)}");
+
+            Console.WriteLine($"Temperature is {_temperature}");
+            Console.WriteLine($"Humidity is {_humidity}");
+            Console.WriteLine($"Pressure is {_pressure}");
+        }
+
+        void IObserver.Update()
+        {
+            _temperature = _weatherData.Temperature;
+            _humidity = _weatherData.Humidity;
+            _pressure = _weatherData.Pressure;
+
+            Display();
+        }
+    }
+}
+
---
+++ Observer/WeatherData.cs
@@ -0,0 +1,49 @@
+﻿namespace Observer
+{
+    public class WeatherData : ISubject
+	{
+        private readonly List<IObserver> _observers;
+        public float Temperature { get; private set; }
+        public float Humidity { get; private set; }
+        public float Pressure { get; private set; }
+
+        public WeatherData()
+		{
+            _observers = new List<IObserver>();
+
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void SetMeasurements(float temperature, float humidity, float pressure)
+        {
+            Temperature = temperature;
+            Humidity = humidity;
+            Pressure = pressure;
+
+            MeasurementsChanged();
+        }
+
+        private void MeasurementsChanged()
+        {
+            NotifyObservers();
+        }
+    }
+}
+
